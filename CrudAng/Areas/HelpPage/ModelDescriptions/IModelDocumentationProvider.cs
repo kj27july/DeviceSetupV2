e280429cff2cff28064559c0f488c039367e0b93
kj27july <79355765+kj27july@users.noreply.github.com>
2021-03-25 19:59:39 +0530

    edit and delete
    Done by me but login is not added

---
+++ CrudAng/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace CrudAng.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ CrudAng/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrudAng.WebApiApplication" Language="C#" %>
